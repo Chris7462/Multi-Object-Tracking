133df79093f7008f702cdc52e7ff1aa7b8d351a8
chris7462 <chris7462@gmail.com>
2023-05-03 15:55:53 -0400

    Start working on the tracking subscriber

--- include/multi_object_tracking/multi_object_tracking.hpp
+++ include/multi_object_tracking/multi_object_tracking.hpp
@@ -2,9 +2,15 @@
 
 // ros header
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
 
 // local ros msg
 #include "tracking_msgs/msg/detected_object_list.hpp"
+
+// local header
+#include "multi_object_tracking/read_param.hpp"
 
 
 class MultiObjectTracking: public rclcpp::Node
@@ -13,8 +19,14 @@ class MultiObjectTracking: public rclcpp::Node
     MultiObjectTracking();
     ~MultiObjectTracking() = default;
 
+    Param param;
+    Trac
   private:
-    void tracking_callback(const tracking_msgs::msg::DetectedObjectList& det_msg);
-
     rclcpp::Subscription<tracking_msgs::msg::DetectedObjectList>::SharedPtr detect_subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr textmarker_publisher_;
+
+    void tracking_callback(const tracking_msgs::msg::DetectedObjectList& det_msg);
 };
--- src/multi_object_tracking.cpp
+++ src/multi_object_tracking.cpp
@@ -7,6 +7,11 @@ MultiObjectTracking::MultiObjectTracking()
 {
   detect_subscription_ = create_subscription<tracking_msgs::msg::DetectedObjectList>(
     "detected_object_list", 10, std::bind(&MultiObjectTracking::tracking_callback, this, std::placeholders::_1));
+
+  img_publisher_ = create_publisher<sensor_msgs::msg::Image>("mot/image", 10);
+  pc_publisher_ = create_publisher<sensor_msgs::msg::PointCloud2>("mot/point_cloud", 10);
+  marker_publisher_ = create_publisher<visualization_msgs::msg::MarkerArray>("mot/box", 10);
+  textmarker_publisher_= create_publisher<visualization_msgs::msg::MarkerArray>("mot/id", 10);
 };
 
 
